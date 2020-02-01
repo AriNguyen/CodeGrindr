5dfb73f97dfcca710b6eea0a8f7207ba1052a070
nvthblake <57298440+nvthblake@users.noreply.github.com>
2020-02-01 07:55:49 -0500

    Added incomplete 4th page

---
+++ CodeGrindr/app/src/main/java/com/example/codegrindr/RegisteredHackActivity.java
@@ -0,0 +1,14 @@
+package com.example.codegrindr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisteredHackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registered_hack);
+    }
+}
