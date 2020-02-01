011ed09dc9c21577fa5e423b3e9e63c012dca3ba
nvthblake <57298440+nvthblake@users.noreply.github.com>
2020-02-01 10:37:37 -0500

    Added Page's Link

--- CodeGrindr/app/src/main/java/com/example/codegrindr/InformationActivity.java
+++ CodeGrindr/app/src/main/java/com/example/codegrindr/InformationActivity.java
@@ -2,9 +2,20 @@ package com.example.codegrindr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class InformationActivity extends AppCompatActivity {
+
+    public void showRegisteredHackActivity() {
+        Intent intent = new Intent(getApplicationContext(), RegisteredHackActivity.class);
+        startActivity(intent);
+    }
+
+    public void gettingStarted(View view) {
+        showRegisteredHackActivity();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
