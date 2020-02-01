ada374005d28ca0f21445c2f342ffd97e2cedadd
Nhi Tran <mainhitranthien@gmail.com>
2020-02-01 10:55:44 -0500

    Connect front end of RegisteredHackActivity.java

--- CodeGrindr/app/src/main/java/com/example/codegrindr/RegisteredHackActivity.java
+++ CodeGrindr/app/src/main/java/com/example/codegrindr/RegisteredHackActivity.java
@@ -3,12 +3,39 @@ package com.example.codegrindr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class RegisteredHackActivity extends AppCompatActivity {
+
+    String[] nameArray = {"Hoya Hacks","Make Harvard","DragonHacks"};
+
+    String[] dateArray = {
+            "Jan 31st - Feb 2nd 2020",
+            "Feb 1st - Feb 2nd 2020",
+            "Feb 22nd - Feb 23rd 2020"
+    };
+
+    String[] locationArray = {
+            "Washington DC, US",
+            "Boston MA, US",
+            "Philadelphia PA, US"
+    };
+
+    Integer[] imageArray = {R.drawable.hoya,
+            R.drawable.makeharvard,
+            R.drawable.dragonhacks
+    };
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registered_hack);
+        setContentView(R.layout.activity_main);
+
+        CustomListAdapter whatever = new CustomListAdapter(this, nameArray, dateArray, locationArray, imageArray);
+        listView = (ListView) findViewById(R.id.registered_hackathon);
+        listView.setAdapter(whatever);
+
     }
 }
