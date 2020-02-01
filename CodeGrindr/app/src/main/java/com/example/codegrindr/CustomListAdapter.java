e442d94e8a6e649e92b0df5f672faecf47c178bf
Nhi Tran <mainhitranthien@gmail.com>
2020-02-01 10:29:23 -0500

    Add location variables to .java

--- CodeGrindr/app/src/main/java/com/example/codegrindr/CustomListAdapter.java
+++ CodeGrindr/app/src/main/java/com/example/codegrindr/CustomListAdapter.java
@@ -18,7 +18,8 @@ public class CustomListAdapter extends ArrayAdapter {
         this.context=context;
         this.imageIDarray = imageIDArrayParam;
         this.nameArray = nameArrayParam;
-        this.infoArray = dateArrayParam;
+        this.dateArray = dateArrayParam;
+        this.locationArray = locationArrayParam;
 
     }
 
@@ -28,13 +29,14 @@ public class CustomListAdapter extends ArrayAdapter {
 
         //this code gets references to objects in the activity_registered_hackathon_row.xml file
         TextView nameTextField = (TextView) rowView.findViewById(R.id.hackathon_name);
-        TextView infoTextField = (TextView) rowView.findViewById(R.id.hackathon_date);
+        TextView dateTextField = (TextView) rowView.findViewById(R.id.hackathon_date);
         TextView locationTextField = (TextView) rowView.findViewById(R.id.hackathon_location);
         ImageView imageView = (ImageView) rowView.findViewById(R.id.imageView);
 
         //this code sets the values of the objects to values from the arrays
         nameTextField.setText(nameArray[position]);
-        infoTextField.setText(infoArray[position]);
+        dateTextField.setText(dateArray[position]);
+        locationTextField.setText(locationArray[position]);
         imageView.setImageResource(imageIDarray[position]);
         return rowView;
 
@@ -47,5 +49,8 @@ public class CustomListAdapter extends ArrayAdapter {
     private final String[] nameArray;
 
     //to store the list of countries
-    private final String[] infoArray;
+    private final String[] dateArray;
+
+    //
+    private final String[] locationArray;
 }
