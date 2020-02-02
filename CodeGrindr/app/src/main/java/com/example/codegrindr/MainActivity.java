b0dea98084e03ab23a277e6a2a339e55ee5369a7
Onlee97 <lekhacduy97@gmail.com>
2020-02-01 19:06:53 -0500

    Login check name, pass, sign up

--- CodeGrindr/app/src/main/java/com/example/codegrindr/MainActivity.java
+++ CodeGrindr/app/src/main/java/com/example/codegrindr/MainActivity.java
@@ -17,42 +17,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button btnSubmit = findViewById(R.id.loginButton);
-        final EditText txtUserName = findViewById(R.id.emailEditText);
-        final EditText txtPassword = findViewById(R.id.passEditText);
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            if (isEmpty(txtUserName)) {
-                txtUserName.setError("You cannot leave this empty");
-            }
-            else if (isEmpty(txtPassword)) {
-                txtPassword.setError("You cannot leave this empty");
-            }
-            else if (checkPasswordLeng(txtPassword)) {
-                txtPassword.setError("Password length should be at least 10 characters");
-                txtPassword.setText("");
-            } else {
-        //final TextView lblUserName = findViewById(R.id.lblUserName);
-        //lblUserName.setText("Works!!");
-                Intent FinshLogin = new Intent(getBaseContext(), RegisteredHackActivity.class);
-                startActivity(FinshLogin);
-            }
-        }
+        final Button loginButton = findViewById(R.id.loginButton);
+        final TextView signUpTextView = findViewById(R.id.signUpTextView);
+        final EditText emailEditText = findViewById(R.id.emailEditText);
+        final EditText passEditText = findViewById(R.id.passEditText);
 
-        private boolean checkPasswordLeng(EditText passText) {
-        if (passText.getText().toString().trim().length() >= 10)
-            return false;
-        return true;
-        }
 
-        private boolean isEmpty(EditText etText) {
-        if (etText.getText().toString().trim().length() > 0)
-            return false;
-        return true;
-        }
-        }
-        );
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isEmpty(emailEditText)) {
+                    emailEditText.setError("You cannot leave this empty");
+                }
+                else if (isEmpty(passEditText)) {
+                    passEditText.setError("You cannot leave this empty");
+                }
+                else if (checkPasswordLeng(passEditText)) {
+                    passEditText.setError("Password length should be at least 10 characters");
+                    passEditText.setText("");
+                } else {
+            //final TextView lblUserName = findViewById(R.id.lblUserName);
+            //lblUserName.setText("Works!!");
+                    Intent FinishLogin = new Intent(getBaseContext(), RegisteredHackActivity.class);
+                    startActivity(FinishLogin);
+                }
+            }
+
+            private boolean checkPasswordLeng(EditText passText) {
+                if (passText.getText().toString().trim().length() >= 10) return false;
+                return true;
+            }
+
+            private boolean isEmpty(EditText etText) {
+                if (etText.getText().toString().trim().length() > 0) return false;
+                return true;
+            }
+        });
+
+        signUpTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent goToSignUp = new Intent(getBaseContext(), SignUpActivity.class);
+                startActivity(goToSignUp);
+            }
+        });
     }
 }
 
